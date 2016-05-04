3ba2b5f254c592fe5a0356e24da92074bc94dfb9
croyboy30 <croy@ipipeline.com>
2016-05-04 13:41:13 -0400

    Test Branch

--- TestJenkins/TestJenkins/Form1.vb
+++ TestJenkins/TestJenkins/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("Hello Jenkins  XXXXXXXXX NEw Message!")
+        MsgBox("Hello Jenkins  AAAAAAAA NEw Message!")
     End Sub
 End Class
