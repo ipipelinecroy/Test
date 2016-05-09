0a6338eefe30456c6ccd045f4f855767a4409c5c
croyboy30 <croy@ipipeline.com>
2016-05-09 09:08:52 -0400

    Changed the message

--- TestJenkins/TestJenkins/Form1.vb
+++ TestJenkins/TestJenkins/Form1.vb
@@ -1,6 +1,6 @@
 ﻿Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-        MsgBox("Hello Jenkins  AAAAAAAA NEw Message!")
+        MsgBox("Hello Jenkins - This is a test Message!")
     End Sub
 End Class
